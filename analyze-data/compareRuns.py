aeaa6afef6b0100feee118911ea10f47c71800f5
Lea Grønning <leagrenning@hotmail.com>
2024-05-06 10:51:20 +0200

    Cleanup

--- analyze-data/compareRuns.py
+++ analyze-data/compareRuns.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def natural_sort_key(s):
     return [int(text) if text.isdigit() else text.lower() for text in re.split('(\d+)', s)]
 
-def compare_json_files(original_folder, upper_bound_folder, interesting_queries):
+def compare_json_files(original_folder, upper_bound_folder, output_file):
     # Lists to store file names and corresponding mean values
     file_names = []
     original_means = []
@@ -22,7 +22,7 @@ def compare_json_files(original_folder, upper_bound_folder, interesting_queries)
         # Check if the file exists in both folders
         if os.path.isfile(original_file_path) and os.path.isfile(upper_bound_file_path):
             # Check if the file has a JSON extension
-            if file_name.endswith('.json') and file_name in interesting_queries:
+            if file_name.endswith('.json'):
                 # Read data from both JSON files
                 with open(original_file_path, 'r') as original_file, open(upper_bound_file_path, 'r') as upper_bound_file:
                     original_data = json.load(original_file)
@@ -48,18 +48,37 @@ def compare_json_files(original_folder, upper_bound_folder, interesting_queries)
 
     # Plotting the bar chart
     plt.figure(figsize=(10, 6))
-    plt.bar(range(len(sorted_file_names)), sorted_original_means, color='blue', alpha=0.5, label='Original')
-    plt.bar(range(len(sorted_file_names)), sorted_upper_bound_means, color='#FFBF00', alpha=0.5, label='Upper bound incr=0.15')
+    firstOriginalTallest = True
+    firstOtherTallest = True
+    for i in range(len(sorted_file_names)):
+        if sorted_original_means[i] > sorted_upper_bound_means[i]:
+            if firstOriginalTallest:
+                plt.bar(i, sorted_original_means[i], color='grey', edgecolor='black', alpha=0.5, label='UpperBound015')
+                plt.bar(i, sorted_upper_bound_means[i], color='black', edgecolor='black', alpha=0.5, hatch='//')
+                firstOriginalTallest = False
+            else:
+                plt.bar(i, sorted_original_means[i], color='grey', edgecolor='black', alpha=0.5)
+                plt.bar(i, sorted_upper_bound_means[i], color='black', edgecolor='black', alpha=0.5, hatch='//')
+        else:
+            if firstOtherTallest:
+                plt.bar(i, sorted_upper_bound_means[i], color='white', edgecolor='black', alpha=0.5, hatch='//', label='G1BoundingBox02')
+                plt.bar(i, sorted_original_means[i], color='black', edgecolor='black', alpha=0.5)
+                firstOtherTallest = False
+            else:
+                plt.bar(i, sorted_upper_bound_means[i], color='white', edgecolor='black', alpha=0.5, hatch='//')
+                plt.bar(i, sorted_original_means[i], color='black', edgecolor='black', alpha=0.5)
+    # plt.bar(range(len(sorted_file_names)), sorted_original_means, color='grey', edgecolor='black', alpha=0.5, label='Original')
+    # plt.bar(range(len(sorted_file_names)), sorted_upper_bound_means, color='white', edgecolor='black', alpha=0.5, label='Other', hatch='//')
     plt.xlabel('Query')
     plt.ylabel('Mean Value (s)')
-    plt.title('Run Time Comparison')
+    plt.title('Run Time Comparison without Histograms')
     plt.xticks(range(len(sorted_file_names)), sorted_file_names, rotation=45, ha='right')
     plt.legend()
     plt.tight_layout()
     plt.show()
 
 # Paths to the folders containing the JSON files and the output Markdown file
-original_folder = "test-results/original/indexes"
+#original_folder = "test-results/original/indexes"
 #original_folder = "test-results/original/histograms_and_indexes"
 
 #original_folder = "test-results/upper_bound_4_levels_incremental_0_15/indexes"
@@ -68,8 +87,8 @@ original_folder = "test-results/original/indexes"
 
 #original_folder = "test-results/bounding_box_alpha_0_5_beta_0_4_gamma_0_1_inc_0_2_dec_0_1/indexes"
 #original_folder = "test-results/bounding_box_alpha_0_5_beta_0_4_gamma_0_1_inc_0_1_dec_0_05/indexes"
-#original_folder = "test-results/upper_bound_4_levels_incremental_0_15/indexes"
-#original_folder = "test-results/upper_bound_4_levels_incremental_0_15/histograms_and_indexes"
+original_folder = "test-results/upper_bound_4_levels_incremental_0_15/indexes"
+#original_folder = "test-results/upper_bound_fixed_0_15/histograms_and_indexes"
 
 #upper_bound_folder = "test-results/naiv_5_percent/indexes"
 #upper_bound_folder = "test-results/naiv_5_percent/histograms_and_indexes"
@@ -77,11 +96,11 @@ original_folder = "test-results/original/indexes"
 #upper_bound_folder = "test-results/naiv_1_percent/histograms_and_indexes"
 #upper_bound_folder = "test-results/upper_bound_4_levels_incremental_0_2/indexes"
 #upper_bound_folder = "test-results/upper_bound_4_levels_incremental_0_2/histograms_and_indexes"
-upper_bound_folder = "test-results/upper_bound_4_levels_incremental_0_15/indexes"
+#upper_bound_folder = "test-results/upper_bound_4_levels_incremental_0_15/indexes"
 #upper_bound_folder = "test-results/upper_bound_4_levels_incremental_0_15/histograms_and_indexes"
 #upper_bound_folder = "test-results/upper_bound_4_levels_incremental_0_1/indexes"
 #upper_bound_folder = "test-results/upper_bound_4_levels_incremental_0_1/histograms_and_indexes"
-#upper_bound_folder = "test-results/bounding_box_alpha_0_5_beta_0_4_gamma_0_1_inc_0_2_dec_0_1/indexes"
+upper_bound_folder = "test-results/bounding_box_alpha_0_5_beta_0_4_gamma_0_1_inc_0_2_dec_0_1/indexes"
 #upper_bound_folder = "test-results/bounding_box_alpha_0_5_beta_0_4_gamma_0_1_inc_0_2_dec_0_1/histograms_and_indexes"
 #upper_bound_folder = "test-results/bounding_box_alpha_0_5_beta_0_4_gamma_0_1_inc_0_15_dec_0_075/indexes"
 #upper_bound_folder = "test-results/bounding_box_alpha_0_5_beta_0_4_gamma_0_1_inc_0_15_dec_0_075/histograms_and_indexes"
@@ -94,9 +113,7 @@ upper_bound_folder = "test-results/upper_bound_4_levels_incremental_0_15/indexes
 #upper_bound_folder = "test-results/bounding_box_alpha_0_7_beta_0_2_gamma_0_1_inc_0_1_dec_0_05/indexes"
 #upper_bound_folder = "test-results/bounding_box_alpha_0_7_beta_0_2_gamma_0_1_inc_0_1_dec_0_05/histograms_and_indexes"
 
-#upper_bound_folder = "results"
-
-interesting_queries = ["10c.json", "28b.json"]
-
 # Call the function to compare JSON files and plot the mean values
-compare_json_files(original_folder, upper_bound_folder, interesting_queries)  # No need for an output file in this case
+compare_json_files(original_folder, upper_bound_folder, None)  # No need for an output file in this case
+
+
--- analyze-data/getAverageExecutionTime.py
+++ analyze-data/getAverageExecutionTime.py
@@ -1,29 +1,3 @@
-# import os
-# import json
-
-# def find_files(directory):
-#     for root, _, files in os.walk(directory):
-#         for file in files:
-#             if file.endswith(".json"):
-#                 yield os.path.join(root, file)
-
-# def check_stddev(file_path):
-#     with open(file_path, 'r') as f:
-#         data = json.load(f)
-#         for result in data['results']:
-#             if type(result['stddev']) is float: 
-#                 if result['stddev'] > 1:
-#                     return True
-#     return False
-
-# def main():
-#     directory = 'test-results'  # Change this to your directory path
-#     for file_path in find_files(directory):
-#         if check_stddev(file_path):
-#             print(file_path)
-
-# if __name__ == "__main__":
-#     main()
 import os
 import json
 
